711da32465f55498fb780d156c5ea758c5a8418e
Kehinde-Agboola <65411917+Kehinde-Agboola@users.noreply.github.com>
2022-12-05 11:03:44 +0100

    Spinners loader

--- src/component/Spinners.jsx
+++ src/component/Spinners.jsx
@@ -4,7 +4,7 @@ import {BounceLoader, BarLoader, BeatLoader} from 'react-spinners'
 const Spinners = () => {
     return(
     <div className="loader__spinner">
-    <BounceLoader size={400} color='green' />
+    <BounceLoader size={200} color='green' />
     </div>
     )
 }
